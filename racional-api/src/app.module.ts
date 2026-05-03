1296f971268135ed0c7053a5ee9a2e811b9472bf
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2026-05-03 01:40:38 -0400

    feat: add Users module, service, and update DTO for user management

--- racional-api/src/app.module.ts
+++ racional-api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { envValidationSchema } from './config/env.validation.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { UsersModule } from './users/users.module.js';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module.js';
       validationSchema: envValidationSchema,
     }),
     PrismaModule,
+    UsersModule,
   ],
 })
 export class AppModule {}
